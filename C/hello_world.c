a10670818020668923b3622cc0495ce606bb4f96
akrom <akrommuhammadyusuf@gmail.com>
2026-08-29 12:05:10 +0700

    Mandiri: Belajar Bahasa C sederhana dan Tipe data dalam C

---
+++ C/hello_world.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int main() 
+{
+    printf("Hello DUnia");
+    return 0;
+}
---
+++ C/tipeData.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main () {
+
+    int angka1 = 10;
+    float angka2 = 10.5;
+    double angka3 = 10.5;
+    char angka4 = 'A';
+
+    printf("%d\n", angka1);
+    printf("%f\n", angka2);
+    printf("%lf\n", angka3);
+    printf("%c\n", angka4);
+
+    return 0;
+}
