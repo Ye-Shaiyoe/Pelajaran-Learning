060e78959cfe4fa684f37259bdf8659433a06efd
akrom <akrommuhammadyusuf@gmail.com>
2026-08-29 12:14:36 +0700

    Mandiri: Belajar Bahasa C sederhana dan Tipe data dalam C perkenalan

--- C/tipeData.c
+++ C/tipeData.c
@@ -2,15 +2,31 @@
 
 int main () {
 
-    int angka1 = 10;
-    float angka2 = 10.5;
-    double angka3 = 10.5;
-    char angka4 = 'A';
+    int angka1 = 10;             //int untuk bilangan bulat
+    float angka2 = 10.5;         // float untuk bilangan desimal presisi rendah
+    double angka3 = 10.5;        // double untuk bilangan desimal presisi tinggi
+    char angka4 = 'A';           // char untuk bilangan karakter
 
-    printf("%d\n", angka1);
-    printf("%f\n", angka2);
-    printf("%lf\n", angka3);
-    printf("%c\n", angka4);
+    printf("Integer : %d\n", angka1);
+    printf("Float : %f\n", angka2);
+    printf("Double : %lf\n", angka3);
+    printf("Char : %c\n", angka4);
 
+    int umur = 18;
+    float tinggi = 165.5;
+    double berat = 45.5; 
+    char nama[10] = "Akrom"; 
+    char hobi[10] = "Muncak"; 
+
+    printf("BIODATA SAYA\n");
+    printf("======================\n"); 
+    printf("Nama : %s\n", nama);
+    printf("Umur : %d\n", umur);
+    printf("Tinggi : %f\n", tinggi);
+    printf("Berat : %lf\n", berat);
+    printf("Hobi: %s\n", hobi); 
+    printf("======================\n"); 
+
+    printf("Yokoso Watashi no Mono\n"); 
     return 0;
 }
